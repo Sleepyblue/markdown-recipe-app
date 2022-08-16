a07f417c9948c0385ae77a3e0c30c07497a6a27c
Sleepyblue <bruno.abs@live.com.pt>
2022-08-16 14:55:07 +0100

    Rough implementation of a holder render method

--- src/js/views/holderView.js
+++ src/js/views/holderView.js
@@ -1,3 +1,15 @@
-class HolderView {}
+class HolderView {
+  _holder = document.querySelector('.recipes__holder');
+
+  renderHolder(recipeTitle) {
+    const markup = `
+        <div class="recipes__holder-recipe">
+            <div>${recipeTitle}</div> 
+        </div>
+       `;
+
+    this._holder.insertAdjacentHTML('beforeend', markup);
+  }
+}
 
 export default new HolderView();
