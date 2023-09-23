e16ac66656595907037b9eebbabcb24f082e6292
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2023-09-23 14:08:12 +0100

    💣 Fix SVG dynamic import
    Why
    - The update from the `vite-plugin-svgr` library broke the previous dynamic SVG importing
    
    How
    - Refactor the `SVGIcon` to import the SVG icons as components
    - Transform the `Close.svg` into a react component
    - Transform the `Cogwheel.svg` into a react component
    
    Tags
     On branch react-revamp
     Changes to be committed:
    	modified:   src/components/atoms/SVGIcon/SVGIcon.tsx
    	deleted:    src/icons/Close.svg
    	new file:   src/icons/Close.tsx
    	deleted:    src/icons/Cogwheel.svg
    	new file:   src/icons/Cogwheel.tsx

--- src/components/atoms/SVGIcon/SVGIcon.tsx
+++ src/components/atoms/SVGIcon/SVGIcon.tsx
@@ -1,5 +1,5 @@
-import { useDynamicSvg } from "../../../hooks/useDynamicSVG.ts";
 import "./SVGIcon.css";
+import { lazy, Suspense } from "react";
 
 type IconButtonProps = {
   iconName: "Cogwheel" | "Close" | "";
@@ -8,31 +8,20 @@ type IconButtonProps = {
 };
 
 function SVGIcon({ iconName, size, iconClass }: IconButtonProps) {
-  const { loading, SvgIcon } = useDynamicSvg(iconName);
+  const iconPath = `../../../icons/${iconName}.tsx`;
+
+  const DynamicIcon = lazy(
+    () =>
+      // To supress vite warning: The above dynamic import cannot be analyzed by Vite.
+      import(iconPath /* @vite-ignore */) as Promise<{
+        default: React.ComponentType<Partial<IconButtonProps>>;
+      }>
+  );
 
   return (
-    <>
-      {loading && <div></div>}
-      {SvgIcon === undefined && !loading && (
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 24 24"
-          >
-            <g fill="none" stroke="currentColor" strokeWidth="1.5">
-              <circle cx="11.5" cy="11.5" r="9.5" opacity=".5" />
-              <path
-                strokeLinecap="round"
-                d="M11.5 15.5a3 3 0 0 1-3-3v-2m3 5a3 3 0 0 0 3-3v-2m-3 5v-5m3 0a3 3 0 1 0-6 0m6 0h-6m6.072 1H16m-9 0h1.5m6 2.5l1 .5m-7-.5l-1 .5m7-5.5l1-.5m-7 .5l-1-.5M20 20l2 2"
-              />
-            </g>
-          </svg>
-        </div>
-      )}
-      {SvgIcon && <SvgIcon width={size} height={size} className={iconClass} />}
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <DynamicIcon size={size} iconClass={iconClass} />
+    </Suspense>
   );
 }
 
---
+++ src/icons/Close.tsx
@@ -0,0 +1,22 @@
+const Close = ({ size, iconClass }: { size: number; iconClass: string }) => {
+  return (
+    <div className={iconClass}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+      >
+        <g fill="none" stroke="currentColor" strokeWidth="1.5">
+          <path
+            d="M2 12c0-4.714 0-7.071 1.464-8.536C4.93 2 7.286 2 12 2c4.714 0 7.071 0 8.535 1.464C22 4.93 22 7.286 22 12c0 4.714 0 7.071-1.465 8.535C19.072 22 16.714 22 12 22s-7.071 0-8.536-1.465C2 19.072 2 16.714 2 12Z"
+            opacity=".5"
+          />
+          <path stroke-linecap="round" d="m14.5 9.5l-5 5m0-5l5 5" />
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default Close;
